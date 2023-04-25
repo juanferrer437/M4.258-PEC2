1bd51e23fb24ab2b6fa4ee14a8d6141c80fdadee
juanferrer437 <juanferrer437@uoc.edu>
2023-04-25 17:16:27 +0200

    a

---
+++ src/assets/scripts/main.js
@@ -0,0 +1,18 @@
+/**
+ * Import dependencies from node_modules
+ * see commented examples below
+ */
+
+// import 'some-node-module';
+// import SomeModule from 'some-node-module';
+
+import 'bootstrap';
+import "@fortawesome/fontawesome-free/js/all";
+/**
+ * Write any other JavaScript below
+ */
+
++( function() {
+  const university = "UOC";
+  console.log(`Hello, ${university}!`);
+} )();
